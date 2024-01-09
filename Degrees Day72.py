de5c3fe38af7e54ae4fa41fb84d537806e47630e
Kabir-5ingh <155265424+Kabir-5ingh@users.noreply.github.com>
2024-01-08 19:15:49 -0500

    Create Degrees Day72.py

---
+++ Degrees Day72.py
@@ -0,0 +1,2 @@
+highest_potential = clean_df.sort_values('Mid-Career 90th Percentile Salary', ascending=False)
+highest_potential[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head()
